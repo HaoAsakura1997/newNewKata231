84133cd755e87d8d4cf0bf3aa5ad54bdad4b90c5
HaoAsakura1997 <96286880+HaoAsakura1997@users.noreply.github.com>
2022-06-14 22:13:04 +0700

    yeah

--- IdeaProjects/FirstSpringBoot/src/main/java/com/haoAsakura97/FirstSpringBoot/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.haoAsakura97.FirstSpringBoot.repository;
-
-import com.haoAsakura97.FirstSpringBoot.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
